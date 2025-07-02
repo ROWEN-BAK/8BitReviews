20b6df43c65e7ee932de4c19c8eeb5e2a31d5f1b
MarkvanderGiessen <9020308@student.zadkine.nl>
2025-07-02 13:31:26 +0200

    uitleg

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-     
+     {/*
+      registreren --> email, username, wachtwoord
+      login --> email & wachtwoord
+      home --> display username en info over de website
+      catalog --> fetch api
+      review --> review schrijven over een willekeurige film
+     */}
     </>
   )
 }
